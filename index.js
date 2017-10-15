6d7263ddfa1516d925b3f54132fd26d4443e1db9
erochOnGit <emile31.roch@gmail.com>
2017-10-15 11:22:25 +0200

    prettify and add prettier to webstorm

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-import Map from "./src/map"
-let app = require('express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+import Map from "./src/map";
+let app = require("express")();
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
 
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/static/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/static/index.html");
 });
 
-
-io.on('connection', function(socket){
-
-    let map = new Map(socket);
-    socket.on('console message', function(msg){
-        if(msg=="getMap") {
-            socket.emit('console message',map.getMap());
-        } else {
-            console.log('message: ' + msg);
-            socket.emit('console message', msg);
-}
+io.on("connection", function(socket) {
+  let map = new Map(socket);
+  socket.on("console message", function(msg) {
+    if (msg == "getMap") {
+      socket.emit("console message", map.getMap());
+    } else {
+      console.log("message: " + msg);
+      socket.emit("console message", msg);
+    }
     //    show map
-        // show village1
-    });
+    // show village1
+  });
 });
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(3000, function() {
+  console.log("listening on *:3000");
 });
--- src/map.js
+++ src/map.js
@@ -1,52 +1,65 @@
 /**
  * Created by emile on 30/09/2017.
  */
-import Village from "./village"
-import RessourseZone from "./ressourceZone"
+import Village from "./village";
+import RessourseZone from "./ressourceZone";
 class Map {
-    constructor(socket){
-        console.log("user connected map generation started");
-        socket.emit('console message', "User connected <br>Map generation started");
-        let village1 = new Village();
-        let village2 = new Village();
-        let village3 = new Village();
-        let champ1 = new RessourseZone();
-        let champ2 = new RessourseZone();
-        let champ3 = new RessourseZone();
-        let champ4 = new RessourseZone();
-        let champ5 = new RessourseZone();
-        let montagne1 = new RessourseZone("&#x39B");
-        let montagne2 = new RessourseZone("&#x39B");
-        let montagne3 = new RessourseZone("&#x39B");
-        let lac1 = new RessourseZone();
-        let lac2 = new RessourseZone();
-        let lac3 = new RessourseZone();
-        let betail1 = new RessourseZone();
-        let betail2 = new RessourseZone();
-        this.heigth = 4;
-        this.width = 4;
-        this.length = 15;
+  constructor(socket) {
+    console.log("user connected map generation started");
+    socket.emit("console message", "User connected <br>Map generation started");
+    let village1 = new Village();
+    let village2 = new Village();
+    let village3 = new Village();
+    let champ1 = new RessourseZone();
+    let champ2 = new RessourseZone();
+    let champ3 = new RessourseZone();
+    let champ4 = new RessourseZone();
+    let champ5 = new RessourseZone();
+    let montagne1 = new RessourseZone("&#x39B");
+    let montagne2 = new RessourseZone("&#x39B");
+    let montagne3 = new RessourseZone("&#x39B");
+    let lac1 = new RessourseZone();
+    let lac2 = new RessourseZone();
+    let lac3 = new RessourseZone();
+    let betail1 = new RessourseZone();
+    let betail2 = new RessourseZone();
+    this.heigth = 4;
+    this.width = 4;
+    this.length = 15;
 
-        this.json={
-            0:montagne1,1:village1,2:champ1,3:lac1,
-            4:betail1,5:champ2, 6:champ3,7:betail2,
-            8:village2,9:lac2,10:montagne2,11:village3,
-            12:champ4,13:montagne3,14:lac3,15:champ5}
-    }
+    this.json = {
+      0: montagne1,
+      1: village1,
+      2: champ1,
+      3: lac1,
+      4: betail1,
+      5: champ2,
+      6: champ3,
+      7: betail2,
+      8: village2,
+      9: lac2,
+      10: montagne2,
+      11: village3,
+      12: champ4,
+      13: montagne3,
+      14: lac3,
+      15: champ5
+    };
+  }
 
-    getMap(){
-        let mapDom = "<div>";
-        for(let i=0;i<=this.length;i++){
-            if(i==0) {
-                mapDom = mapDom + "<div>"
-            }else if(i%this.width==0){
-                mapDom = mapDom + "</div><br><div>"
-            }
-                console.log(i);
-                mapDom = mapDom + "<span>" + this.json[i].character + "&#32&#32</span>"
-        }
-        mapDom = mapDom + "</div>";
-        return mapDom
+  getMap() {
+    let mapDom = "<div>";
+    for (let i = 0; i <= this.length; i++) {
+      if (i == 0) {
+        mapDom = mapDom + "<div>";
+      } else if (i % this.width == 0) {
+        mapDom = mapDom + "</div><br><div>";
+      }
+      console.log(i);
+      mapDom = mapDom + "<span>" + this.json[i].character + "&#32&#32</span>";
     }
+    mapDom = mapDom + "</div>";
+    return mapDom;
+  }
 }
-export default Map ;
+export default Map;
